84ac995263cd37f6d52b865b176540a17566ee5a
CristianMadureira <cristian.madureira.c@gmail.com>
2021-12-30 15:42:09 -0300

    Cria botao cancelar com navegaçao

--- app/src/main/java/com/example/listadecompras/fragments/InsertProductFragment.kt
+++ app/src/main/java/com/example/listadecompras/fragments/InsertProductFragment.kt
@@ -2,12 +2,15 @@ package com.example.listadecompras.fragments
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.example.listadecompras.R
 import com.example.listadecompras.databinding.FragmentAddProductBinding
 import com.example.listadecompras.model.MyListViewModel
@@ -23,29 +26,31 @@ class InsertProductFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        setHasOptionsMenu(true)
-
         binding = FragmentAddProductBinding.inflate(layoutInflater, container, false)
         val root = binding.root
 
+        setHasOptionsMenu(true)
         val menuHost: MenuHost = requireActivity()
 
         menuHost.addMenuProvider(object: MenuProvider{
             override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                menuInflater.inflate(R.menu.menu, menu)
+                menuInflater.inflate(R.menu.menu_check_product, menu)
             }
 
             override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
                 return when (menuItem.itemId){
                     R.id.add_confirme -> {
                         buttonCheckListner()
-                        buttonCheckNavigateTo()
+                        buttonNavigateTo()
                         true
                     }
+                   R.id.cancel_add -> {
+                       buttonNavigateTo()
+                       true
+                   }
                     else -> onOptionsItemSelected(menuItem)
                 }
             }
-
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
 
         return root
@@ -55,7 +60,7 @@ class InsertProductFragment: Fragment() {
         viewModel.addNewProduct(binding.editName.text.toString(), binding.editPrice.text.toString(), binding.editQuantity.text.toString())
     }
 
-    fun buttonCheckNavigateTo(){
+    fun buttonNavigateTo(){
         findNavController().navigate(R.id.action_insertProductFragment_to_homeScreenFragment)
     }
 }
